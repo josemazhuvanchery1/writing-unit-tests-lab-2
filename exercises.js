f4b14127145e96da2f6042a57ed5e86e13c85ed7
josemazhuvanchery1 <jose1998mathew@gmail.com>
2023-01-25 12:28:04 -0500

    finished

--- exercises.js
+++ exercises.js
@@ -1,17 +1,31 @@
-function octalToDecimal() {
-  //Write code here
+function octalToDecimal(stringNum) {
+  let result = 0;
+  let count = stringNum.length-1;
+  for(let i =0; i< stringNum.length; i++){
+    result+= stringNum[i] * (Math.pow(8,count))
+    count--
+  }
+  return result
 }
 
-function anagram() {
-  //Write code here
+function anagram(word, arrOfWords) {
+  return arrOfWords.filter(el => el.split('').sort().join('') === word.split('').sort().join(''));
 }
 
-function triangle() {
-  //Write code here
+function triangle(a, b, c) {
+  return (a === b || b === c || a * b * c === 0) ? "invalid" :  (a===90 || b===90||c==90) ? "right" : a<=90 && b<=90 && c<=90 ? "acute" : a>=90 || b>=90||c>=90 ? "obtuse" : "what is a triangle";
 }
 
-function fridayThe13ths() {
-  //Write code here
+function fridayThe13ths(year) {
+  let counter = 0;
+  for (let i = 1; i <= 12; i++) {
+    let day = new Date(i + "/13/" + year);
+    if (day.getDay() === 5) {
+      counter++;
+    }
+  }
+  return counter;
+
 }
 
 module.exports = {
